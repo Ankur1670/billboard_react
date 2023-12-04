883c02328c4e54070405764bc51e221e3ed4aaea
Yash <yashrasniya3@gmail.com>
2023-12-04 18:43:52 +0530

    some

--- src/Component/imgsec/Imgsec.jsx
+++ src/Component/imgsec/Imgsec.jsx
@@ -19,21 +19,31 @@ const Imgsec = () => {
     const [city,setCity]=useState()
     const [cities,setCities]=useState([])
     const [location,setLocation]=useState()
-    const handelChange=(obj)=>{
-        var st='';
-
-            setCities([])
-        console.log(cities)
-            const a=cc.map((obj2)=>{
-                if (obj2.includes(obj.target.value)){
-                    // if ()
-                    setCities([...cities,obj2])
+    const [locations,setLocations]=useState([])
+    const handelSuggest = (obj) => {
+        let list=[]
+        if (obj.target.value.length>0){
+            cc.map((obj2)=>{
+                if (obj2.toLowerCase().includes(obj.target.value.toLowerCase())){
+                    list.push(obj2)
                 }
             })
-
-
+        }
+      return list
+    }
+    const handelChangeCities=(obj)=>{
+        const list =handelSuggest(obj)
+        setCities(list)
+        setCity(obj.target.value)
 
     }
+    const handelChangeLocation=(obj)=>{
+        const list =handelSuggest(obj)
+        setLocations(list)
+        setLocation(obj.target.value)
+
+    }
+
 
 
   return (
@@ -50,8 +60,11 @@ const Imgsec = () => {
   <div class="form-outline" data-mdb-input-init>
       <label className="form-label" htmlFor="form1">Find Your Desired City</label>
 
-      <input type="search" id="city" class="form-control" onChange={handelChange} value={city}/>
-      <div id={'cities'}>{cities.map((obj)=><a>{obj}</a>)}</div>
+      <input type="search" id="city" class="form-control" onChange={handelChangeCities} value={city}/>
+      <div id={'cities'}>{cities.map((obj)=><a onClick={()=>{
+          setCity(obj)
+          setCities([])
+      }}>{obj}</a>)}</div>
 
   </div>
   
@@ -62,17 +75,21 @@ const Imgsec = () => {
                                 <div className="col-lg-4">
                                 <div class="input-group">
   <div class="form-outline" data-mdb-input-init>
-    <input type="search" id="form1" class="form-control"  value={location}/>
-    <label class="form-label" for="form1">Find Your Desired location</label>
-  
+      <label className="form-label" htmlFor="form1">Find Your Desired location</label>
+
+      <input type="search" id="form1" class="form-control" onChange={handelChangeLocation} value={location}/>
+      <div id={'cities'}>{locations.map((obj)=><a onClick={()=>{
+          setLocation(obj)
+          setLocations([])
+      }}>{obj}</a>)}</div>
+
                                 </div>
                         </div>
                     </div>
                     <div className="col-lg-4">
                     <div class="input-group">
-  <div class="form-outline" data-mdb-input-init>
-    <input id="search-focus" type="search" id="form1" class="form-control" />
-    <label class="form-label" for="form1">Search</label>
+  <div class="form-outline" data-mdb-input-init>    
+    <button className={'button'}>Search</button>
   </div>
   
 </div>
