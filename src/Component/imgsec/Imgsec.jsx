fa6deca0834cb27753ab5c79d4dcffc33ebc2406
Yash <yashrasniya3@gmail.com>
2023-12-04 17:44:39 +0530

    some

--- src/Component/imgsec/Imgsec.jsx
+++ src/Component/imgsec/Imgsec.jsx
@@ -9,8 +9,33 @@ import { FaStarHalfAlt } from "react-icons/fa";
 // import StarRatings from './react-star-ratings';
 
 import { useState } from 'react';
+const cc=[
+    'Dehradun',
+    'goa',
+
+]
 const Imgsec = () => {
     const [value,setValue]=useState([])
+    const [city,setCity]=useState()
+    const [cities,setCities]=useState([])
+    const [location,setLocation]=useState()
+    const handelChange=(obj)=>{
+        var st='';
+
+            setCities([])
+        console.log(cities)
+            const a=cc.map((obj2)=>{
+                if (obj2.includes(obj.target.value)){
+                    if ()
+                    setCities([...cities,obj2])
+                }
+            })
+
+
+
+    }
+
+
   return (
     <div className="imgSec">
     <img src={img1} alt="" />
@@ -23,9 +48,11 @@ const Imgsec = () => {
                             <div className="col-lg-4">
                             <div class="input-group">
   <div class="form-outline" data-mdb-input-init>
-    <input type="search" id="form1" class="form-control" />
-    <label class="form-label" for="form1">Find Your Desired City</label>
-  
+      <label className="form-label" htmlFor="form1">Find Your Desired City</label>
+
+      <input type="search" id="city" class="form-control" onChange={handelChange} value={city}/>
+      <div id={'cities'}>{cities.map((obj)=><a>{obj}</a>)}</div>
+
   </div>
   
 </div>
@@ -35,8 +62,8 @@ const Imgsec = () => {
                                 <div className="col-lg-4">
                                 <div class="input-group">
   <div class="form-outline" data-mdb-input-init>
-    <input type="search" id="form1" class="form-control" />
-    <label class="form-label" for="form1">Find Your Desired City</label>
+    <input type="search" id="form1" class="form-control"  value={location}/>
+    <label class="form-label" for="form1">Find Your Desired location</label>
   
                                 </div>
                         </div>
